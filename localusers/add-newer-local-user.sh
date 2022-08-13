f427ba144f4e23766bd85ad4cb795849ea95f2f7
inspiro-sk <development@inspiro.sk>
2022-08-13 23:15:51 +0200

    Deletion script completed

--- localusers/add-newer-local-user.sh
+++ localusers/add-newer-local-user.sh
@@ -29,7 +29,7 @@ COMMENT="${@}"
 # Automatically generates a password for the new account.
 S='!@#$%^&*()_+{}:<>?'
 SHUFFLED=$(echo "${S}" | fold -w1 | shuf | head -c1)
-PASSWORD="$(echo ${USER_NAME}${RANDOM}$(date +s%) | sha256sum | head -c15)${SHUFFLED}"
+PASSWORD="$(echo ${USER_NAME}${RANDOM}$(date +s%) | sha256sum | head -c7)${SHUFFLED}"
 
 # Informs the user if the account was not able to be created for some reason.
 # If the account is not created, the script is to return an exit status of 1.
@@ -41,7 +41,8 @@ if [[ "${?}" -ne 0 ]]; then
 	exit 1
 fi
 
-echo "${PASSWORD}" | passwd -e --stdin ${USER_NAME} &> /dev/null
+echo "${PASSWORD}" | passwd --stdin ${USER_NAME} &> /dev/null
+passwd -e ${USER_NAME}
 
 # Displays the username, password, and host where the account was created.
 # This way the help desk staff can copy the output of the script in order to 
--- localusers/luser-demo_6_04.sh
+++ localusers/luser-demo_6_04.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+# Define a function to display error message of any kind
+function error_log {
+    local MESSAGE="${@}"
+    echo "${MESSAGE}" >&2
+    exit 1
+}
+
 # Enforces that it be executed with superuser (root) privileges.  If the script
 # is not executed with superuser privileges it will not attempt to create a
 # user and returns an exit status of 1.  All messages associated with this
 # event will be displayed on standard error.
 if [[ "${UID}" -ne "0" ]]
 then
-	echo "You need to run this script with sudo or as root." >&2
-	exit 1
+	error_log "You need to run this script with sudo or as root."
 fi
 
 # Provides a usage statement much like you would find in a man page if the user
@@ -33,27 +39,100 @@ fi
 # Any other option will cause the script to display a usage statement and exit
 # with an exit status of 1.
 
-while getopts dra OPTION:
+# Let's define usage function that echos a manual like page:
+function usage {
+    echo
+    echo "Usage: ${0} [-d] [-r] [-a] USER_NAME [USER_NAME]"
+    echo
+    echo 'Disable specified user(s) from the system. Disabled users cannot log in.'
+    echo
+    echo 'Options:'
+    echo '  -d    Delete user instead of disabling.'
+    echo '  -a    Create archive of user''s home directory'
+    echo '  -r    Remove home directory associated with user account'
+    echo
+    exit 1
+}
+
+# define defaults
+DISABLE_FLAG='true'
+REMOVE_HOME_FLAG='false'
+ARCHIVE_FLAG='false'
+
+while getopts dra OPTION
 do
 	case ${OPTION} in
 		d)
 			# delete account instead of disabling it
-			
+			echo "User account will be deleted instead of disabling"
+            DISABLE_FLAG='false'
 			;;
 		r)
 			# remove home directory
-			REMOVE_HOME_FLAG='r'
+            echo "Removing /home/${USER_NAME} - this operation cannot be undone."
+			REMOVE_HOME_FLAG='true'
 			;;
 		a)
 			# archive home directory
+            echo "Archiving /home/${USER_NAME} into /var/tmp/restore/${USER_NAME}.tar.gz"
+            ARCHIVE_FLAG='true'
+            ;;
+    esac
+done
+
+function delete_or_disable_user {
+    if [[ "${DISABLE_FLAG}" = 'true' ]]
+    then
+        echo "Disabling user ${2} with UID ${3}"
+        chage -E0 ${USER_NAME} 
+    else
+        echo "Deleting user ${2} with UID ${3}"
+        userdel -f ${USER_NAME}
+    fi
+}
+
+function archive_homedir {
+    if [[ "${ARCHIVE_FLAG}" = 'true' ]]
+    then
+        tar -cvzf /var/tmp/restore/${USER_NAME}.tar.gz /home/${USER_NAME}
+    fi
+}
+
+function remove_homedir {
+    if [[ "${REMOVE_HOME_FLAG}" = 'true' ]]
+    then
+        rm -rf /home/${USER_NAME}
+    fi
+}
 
 # Accepts a list of usernames as arguments.  At least one username is required
 # or the script will display a usage statement much like you would find in a man
 # page and return an exit status of 1.  All messages associated with this event
 # will be displayed on standard error.
 
-# Refuses to disable or delete any accounts that have a UID less than 1,000.
+# shift to arguments passed after options:
+shift $(( $OPTIND - 1))
 
+# check if at least one username has been passed
+if [[ "${#}" -eq 0 ]]
+then
+    usage
+    exit 1
+fi
+
+for USER_NAME in "${@}"
+do
+# Refuses to disable or delete any accounts that have a UID less than 1,000.
+    USER_ID=$(id -u $USER_NAME)
+    if [[ "${USER_ID}" -lt "1000" ]]
+    then
+        :
+    else
+        delete_or_disable_user ${DISABLE_FLAG} ${USER_NAME} ${USER_ID}
+        archive_homedir ${ARCHIVE_FLAG} ${USER_NAME} ${USER_ID}
+        remove_homedir ${ARCHIVE_FLAG} ${USER_NAME} ${USER_ID}
+    fi
+done
 # Only system accounts should be modified by system administrators.  Only allow
 # the help desk team to change user accounts.
 
