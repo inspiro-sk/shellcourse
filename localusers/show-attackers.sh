a925b8261888906e7a3650fc6781423534d3e5f4
inspiro-sk <development@inspiro.sk>
2022-08-16 09:00:09 +0200

    show-attackers geoiplookup done

--- localusers/show-attackers.sh
+++ localusers/show-attackers.sh
@@ -9,4 +9,18 @@ then
 fi
 
 # parse the syslog to get list of IPs
-grep 'Failed' ${INPUT_FILE} | cut -d ':' -f 4 | awk -F ' ' '{print $(NF-3)}' | sort | uniq -c | sort -nr
+# redirect to /dev/null as this is just for illustration
+# part of this script logis is used to get IP_LIST and use
+# geoiplookup to determine country where the traffic is inbound from
+grep 'Failed' ${INPUT_FILE} | cut -d ':' -f 4 | awk -F ' ' '{print $(NF-3)}' | sort | uniq -c | sort -nr >& /dev/null
+
+# determine list of unique IPs
+IP_LIST=$(grep 'Failed' ${INPUT_FILE} | cut -d ':' -f 4 | awk -F ' ' '{print $(NF-3)}' | sort | uniq)
+
+# lookup the country of origin
+for IP in ${IP_LIST}
+do
+    echo "${IP} | $(geoiplookup ${IP})"
+done
+
+exit 0
